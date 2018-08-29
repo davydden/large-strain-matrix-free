53ee6b62453f6045d1139c8799a607d213d0c729
Denis Davydov <davydden@gmail.com>
2018-08-29 07:51:11 +0200

    incrase requested time more (7 hours)

--- Calculations/emmy_benchmark_intel.sh
+++ Calculations/emmy_benchmark_intel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#   allocate 1 nodes with 40 CPU per node for 6 hours:
-#PBS -l nodes=1:ppn=40,walltime=6:00:00
+#   allocate 1 nodes with 40 CPU per node for 7 hours:
+#PBS -l nodes=1:ppn=40,walltime=7:00:00
 #   job name
-#PBS -N dealii_large_strain
+#PBS -N dealii_mf_cm
 #   stdout and stderr files:
 #PBS -o /home/woody/iwtm/iwtm108/deal.ii-mf-elasticity/_build/bench.txt -e /home/woody/iwtm/iwtm108/deal.ii-mf-elasticity/_build/bench_error.txt
 #   first non-empty non-comment line ends PBS options
