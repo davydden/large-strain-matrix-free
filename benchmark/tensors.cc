0a2e6bf9aee3e4503ad26ae3a6a0c6982af5dacf
Denis Davydov <davydden@gmail.com>
2019-02-01 14:10:35 +0100

    implement various tensor2 contractions

--- benchmark/tensors.cc
+++ benchmark/tensors.cc
@@ -43,23 +43,30 @@ test(const unsigned int n_cells = 22528) // 90112 active cells in 2D for p=4 =>
             << "n_cell_batches     = " << n_cells << std::endl
             << "n_q_points         = " << n_q_points << std::endl
             << "number of products = " << n_cells * n_q_points << std::endl
-            << "multiplier         = " // Runtime * MFLOP/s * multiplier = FLOP for a single operation
-            << 1000000. / (n_cells * n_q_points *
-                           VectorizedArray<number>::n_array_elements)
+            << "multiplier         = "
+            << 1000000. /
+                 (n_cells * n_q_points) // Runtime * MFLOP/s * multiplier = FLOP
+                                        // for a single operation
             << std::endl;
 
   Table<2, Tensor<2, dim, VectorizedArray<number>>>          cached_tensor2;
+  Table<2, SymmetricTensor<2, dim, VectorizedArray<number>>> cached_tensor2sym;
   Table<2, SymmetricTensor<4, dim, VectorizedArray<number>>> cached_tensor4;
 
   cached_tensor2.reinit(n_cells, n_q_points);
   cached_tensor4.reinit(n_cells, n_q_points);
+  cached_tensor2sym.reinit(n_cells, n_q_points);
 
   // set tensors to something (does not matter)
   const VectorizedArray<number> one = make_vectorized_array(1.);
+  const VectorizedArray<number> zero = make_vectorized_array(0.);
+
   for (unsigned int cell = 0; cell < n_cells; ++cell)
     for (unsigned int q = 0; q < n_q_points; ++q)
       {
         cached_tensor2(cell, q) =
+          one * Physics::Elasticity::StandardTensors<dim>::I;
+        cached_tensor2sym(cell, q) =
           one * Physics::Elasticity::StandardTensors<dim>::I;
         cached_tensor4(cell, q) =
           one * Physics::Elasticity::StandardTensors<dim>::S +
@@ -95,6 +102,8 @@ test(const unsigned int n_cells = 22528) // 90112 active cells in 2D for p=4 =>
   timer.leave_subsection();
 
   // now test 2-nd order tensors
+
+  // OPTION1 : operator*
   timer.enter_subsection("tensor2");
   LIKWID_MARKER_START("tensor2");
   for (unsigned int cell = 0; cell < n_cells; ++cell)
@@ -105,6 +114,83 @@ test(const unsigned int n_cells = 22528) // 90112 active cells in 2D for p=4 =>
           grad_Nx_v * cached_tensor2(cell, q);
       }
   LIKWID_MARKER_STOP("tensor2");
+  timer.leave_subsection();
+
+  // OPTION2: contract<1,0>
+  timer.enter_subsection("tensor2_c10");
+  LIKWID_MARKER_START("tensor2_c10");
+  for (unsigned int cell = 0; cell < n_cells; ++cell)
+    for (unsigned int q = 0; q < n_q_points; ++q)
+      {
+        // the results which goes to FEEvaluation::submit_gradient()
+        const Tensor<2, dim, VectorizedArray<number>> res =
+          contract<1,0>(grad_Nx_v, cached_tensor2(cell, q));
+      }
+  LIKWID_MARKER_STOP("tensor2_c10");
+  timer.leave_subsection();
+
+  // OPTION3: contract<1,1>
+  timer.enter_subsection("tensor2_c11");
+  LIKWID_MARKER_START("tensor2_c11");
+  for (unsigned int cell = 0; cell < n_cells; ++cell)
+    for (unsigned int q = 0; q < n_q_points; ++q)
+      {
+        // the results which goes to FEEvaluation::submit_gradient()
+        const Tensor<2, dim, VectorizedArray<number>> res =
+          contract<1,1>(grad_Nx_v, cached_tensor2(cell, q)); // use the fact that \tau is symmetric
+      }
+  LIKWID_MARKER_STOP("tensor2_c11");
+  timer.leave_subsection();
+
+  // OPTION3: manual
+  timer.enter_subsection("tensor2_manual");
+  LIKWID_MARKER_START("tensor2_manual");
+  for (unsigned int cell = 0; cell < n_cells; ++cell)
+    for (unsigned int q = 0; q < n_q_points; ++q)
+      {
+        // the results which goes to FEEvaluation::submit_gradient()
+        Tensor<2, dim, VectorizedArray<number>> res;
+        const Tensor<2, dim, VectorizedArray<number>> & rhs = cached_tensor2(cell, q);
+        for (unsigned int i = 0; i < dim; ++i)
+          {
+            auto & res_i = res[i];
+            auto & grad_Nx_v_i = grad_Nx_v[i];
+            for (unsigned int j = 0; j < dim; ++j)
+              {
+                auto & res_ij = res_i[j];
+                auto & rhs_j = rhs[j];
+                res_ij = zero;
+                for (unsigned int k = 0; k < dim; ++k)
+                  res_ij += grad_Nx_v_i[k] * rhs_j[k]; // rhs is symmetric, actually
+              }
+          }
+      }
+  LIKWID_MARKER_STOP("tensor2_manual");
+  timer.leave_subsection();
+
+  // OPTION4: manual symmetric
+  timer.enter_subsection("tensor2_manual_sym");
+  LIKWID_MARKER_START("tensor2_manual_sym");
+  for (unsigned int cell = 0; cell < n_cells; ++cell)
+    for (unsigned int q = 0; q < n_q_points; ++q)
+      {
+        // the results which goes to FEEvaluation::submit_gradient()
+        Tensor<2, dim, VectorizedArray<number>> res;
+        const SymmetricTensor<2, dim, VectorizedArray<number>> & rhs = cached_tensor2sym(cell, q);
+        for (unsigned int i = 0; i < dim; ++i)
+          {
+            auto & res_i = res[i];
+            auto & grad_Nx_v_i = grad_Nx_v[i];
+            for (unsigned int j = 0; j < dim; ++j)
+              {
+                auto & res_ij = res_i[j];
+                res_ij = zero;
+                for (unsigned int k = 0; k < dim; ++k)
+                  res_ij += grad_Nx_v_i[k] * rhs[j][k]; // rhs is symmetric, actually
+              }
+          }
+      }
+  LIKWID_MARKER_STOP("tensor2_manual_sym");
   timer.leave_subsection();
 
   LIKWID_MARKER_CLOSE;
